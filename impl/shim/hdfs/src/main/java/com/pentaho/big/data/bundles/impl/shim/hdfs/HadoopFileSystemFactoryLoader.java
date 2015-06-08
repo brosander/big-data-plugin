da5b3b145c02793e2db89fd89c66d60a2aeded32
Bryan Rosander <brosander@pentaho.com>
2015-06-08 17:20:28 -0400

    wip

--- impl/shim/hdfs/src/main/java/com/pentaho/big/data/bundles/impl/shim/hdfs/HadoopFileSystemFactoryLoader.java
+++ impl/shim/hdfs/src/main/java/com/pentaho/big/data/bundles/impl/shim/hdfs/HadoopFileSystemFactoryLoader.java
@@ -2,11 +2,10 @@ package com.pentaho.big.data.bundles.impl.shim.hdfs;
 
 import com.pentaho.big.data.bundles.impl.shim.common.ShimBridgingClassloader;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceEvent;
-import org.osgi.framework.ServiceListener;
 import org.osgi.framework.ServiceRegistration;
 import org.pentaho.bigdata.api.hdfs.HadoopFileSystemFactory;
 import org.pentaho.di.core.hadoop.HadoopConfigurationBootstrap;
+import org.pentaho.di.core.hadoop.HadoopConfigurationListener;
 import org.pentaho.hadoop.shim.ConfigurationException;
 import org.pentaho.hadoop.shim.HadoopConfiguration;
 import org.slf4j.Logger;
@@ -19,7 +18,7 @@ import java.util.Map;
 /**
  * Created by bryan on 6/4/15.
  */
-public class HadoopFileSystemFactoryLoader {
+public class HadoopFileSystemFactoryLoader implements HadoopConfigurationListener {
   public static final String HADOOP_FILESYSTEM_FACTORY_IMPL_CANONICAL_NAME =
     HadoopFileSystemFactoryImpl.class.getCanonicalName();
   private static final Logger LOGGER = LoggerFactory.getLogger( HadoopFileSystemFactoryLoader.class );
@@ -29,23 +28,20 @@ public class HadoopFileSystemFactoryLoader {
 
   public HadoopFileSystemFactoryLoader( BundleContext bundleContext ) throws ConfigurationException {
     this.bundleContext = bundleContext;
-    HashMap properties = new HashMap();
-    properties.put( "active", true );
-    hadoopConfigurationAdded( HadoopConfigurationBootstrap.getInstance().getProvider().getActiveConfiguration(),
-      properties );
+    HadoopConfigurationBootstrap.getInstance().registerHadoopConfigurationListener( this );
   }
 
-  public void hadoopConfigurationAdded( HadoopConfiguration hadoopConfiguration, Map properties ) {
+  @Override public void onConfigurationOpen( HadoopConfiguration hadoopConfiguration, boolean defaultConfiguration ) {
     if ( hadoopConfiguration == null ) {
       return;
     }
-    boolean activeShim = properties != null && Boolean.valueOf( String.valueOf( properties.get( "active" ) ) );
     ShimBridgingClassloader shimBridgingClassloader =
       new ShimBridgingClassloader( hadoopConfiguration.getHadoopShim().getClass().getClassLoader(), bundleContext );
     try {
       HadoopFileSystemFactory hadoopFileSystemFactory = (HadoopFileSystemFactory) Class
         .forName( HADOOP_FILESYSTEM_FACTORY_IMPL_CANONICAL_NAME, true, shimBridgingClassloader )
-        .getConstructor( boolean.class, HadoopConfiguration.class ).newInstance( activeShim, hadoopConfiguration );
+        .getConstructor( boolean.class, HadoopConfiguration.class )
+        .newInstance( defaultConfiguration, hadoopConfiguration );
       ServiceRegistration hadoopFileSystemFactoryServiceRegistration =
         bundleContext.registerService( HadoopFileSystemFactory.class, hadoopFileSystemFactory, new Hashtable<String,
           Object>() );
@@ -56,11 +52,12 @@ public class HadoopFileSystemFactoryLoader {
     }
   }
 
-  public void hadoopConfigurationRemoved( HadoopConfiguration hadoopConfiguration, Map properties ) {
+  @Override public void onConfigurationClose( HadoopConfiguration hadoopConfiguration ) {
     if ( hadoopConfiguration == null ) {
       return;
     }
-    ServiceRegistration hadoopFileSystemFactoryServiceRegistration = hadoopFileSystemFactoryMap.remove( hadoopConfiguration );
+    ServiceRegistration hadoopFileSystemFactoryServiceRegistration =
+      hadoopFileSystemFactoryMap.remove( hadoopConfiguration );
     if ( hadoopFileSystemFactoryServiceRegistration != null ) {
       hadoopFileSystemFactoryServiceRegistration.unregister();
     }
