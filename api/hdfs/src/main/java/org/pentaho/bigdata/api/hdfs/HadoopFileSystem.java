aa298f8c3d3ab208ecfe78683e36633e75106cf9
Bryan Rosander <brosander@pentaho.com>
2015-07-22 16:57:06 -0400

    wip

--- api/hdfs/src/main/java/org/pentaho/bigdata/api/hdfs/HadoopFileSystem.java
+++ api/hdfs/src/main/java/org/pentaho/bigdata/api/hdfs/HadoopFileSystem.java
@@ -29,7 +29,11 @@ public interface HadoopFileSystem {
 
   HadoopFileSystemPath resolvePath( HadoopFileSystemPath sharedObjectsPath );
 
+  HadoopFileSystemPath makeQualified( HadoopFileSystemPath hadoopFileSystemPath );
+
   void chmod( HadoopFileSystemPath hadoopFileSystemPath, int permissions );
 
   boolean exists( HadoopFileSystemPath path );
+
+  String getFsDefaultName();
 }
